29d28075b88f8baf5eda04b62949c00497c96a57
jsonwang2003 <jason0100w@gmail.com>
2025-07-28 22:04:54 -0700

    Clearned up Face Detection Code

--- Face_Recog.py
+++ Face_Recog.py
@@ -5,26 +5,28 @@ import face_recognition_models
 import serial
 import time
 
-# img = cv.imread('Photos\Caleb3.jpg')
+# Function to send serial data to metroboard
+def send_command(cmd):
+    # Send boolean command to metroboard as 'True' or 'False' string
+    if cmd:
+        msg = 'True\n'
+    else:
+        msg = 'False\n'
+    ser.write(msg.encode('ascii'))
+    ser.flush()
+    time.sleep(0.1)  # short delay
 
-# cv.imshow('Caleb', img)
-# cv.waitKey(0)
-
-# image = face_recognition.load_image_file("Photos\Caleb3.jpg")
-# face_locations = face_recognition.face_locations(image)
-# for box in face_locations:
-#     top, right, bottom, left = box
-#     print("Face at:", top, right, bottom, left)
-# cv.rectangle(image, (left, top), (right, bottom), (0, 255, 0), thickness = 2)
-# cv.imshow('Identify', image)
+    # read metro1 response (if any)
+    while ser.in_waiting > 0:
+        response = ser.readline().decode('utf-8').strip()
+        print("metro1 says:", response)
+        if response == "Time out":
+            print("Lock door")
+        else:
+            print("Unlock door")
 
 # Start video capture
 capture = cv.VideoCapture(0)
-
-
-# Captures the video of first video tool
-capture=cv.VideoCapture(0)
-
 
 # Load images to look for
 # Need to make photos folder and add photos you want to check
@@ -37,100 +39,55 @@ known_face_encodings = [
     caleb_face_encoding
 ]
 
-
 # Add multiple names to the list
 face_names = ["Caleb"]
 
-
 ser = serial.Serial(port='COM16', baudrate= 115200,timeout = 1)
-
-time.sleep(2)
-
-unlocked = False
-
-def send_command(cmd):
-
-     # send command with newline
-    ser.write((cmd + '\n').encode('ascii'))
-    ser.flush()
-    time.sleep(0.1)  # short delay
-
-    # read metro1 response (if any)
-    while ser.in_waiting > 0:
-        response = ser.readline().decode('utf-8').strip()
-        print("metro1 says:", response)
-        if response == "Time out":
-            print("Change false")
-            unlocked = False
-        else:
-            print("Change true")
-            unlocked = True
-
-# def read_serial():
-#     # read metro1 response (if any)
-#     while ser.in_waiting > 0:
-#         response = ser.readline().decode('utf-8').strip()
-#         print("metro1 says:", response)
-#         if response == "Time out":
-#             unlocked = False
-#             print("Changed to false")
-#         else:
-#             unlocked = True
-#             print("Changed to true")
-
-# read_serial()
-
-time.sleep(1)
 
 process_this_frame = True
 
 # Start Face Detection, capturing frame-by-frame
 while True:
-
-    # While loop is running capture every frame and process
+    # Grab a single frame of video
     ret, frame = capture.read()
-
+    
+    # Only Process every other frame to save time
     if process_this_frame:
-        
         # Resize frame of video to 1/4 size for faster face recognition processing
         small_frame = cv.resize(frame, (0, 0), fx=0.25, fy=0.25)
-        # Convert frame taken to RGB
+
+        # Convert the image from BGR (OpenCV) to RGB (face_recognition)
         rgb_frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
-        # Use face location function to get face location of the frame
+
+        # Find all the faces and face encodings in the current frame of video
         face_locations = face_recognition.face_locations(rgb_frame)
-        # Use face encoding function to get the face encoding of the frame
         face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
 
-    # For loop with 3 different variables
-    # top, right, bottom, left, takes the top left coordinate and bottom right coordinate of a rectangle that covers the face location for every loop
-    # Face_encoding loops through the array of face_encodings and known loops through the array of known_face_encodings
+        face_names = []
+        for face_encoding in face_encodings:
+            # Compare the face encoding with the known faces
+            matches = face_recognition.compare_faces(known_face_encodings, face_encoding, tolerance=0.42)
+            name = "Unknown"
+
+            # If a match is found, use the first one
+            if True in matches:
+                first_match_index = matches.index(True)
+                name = face_names[first_match_index]
+                send_command("True")
+            else:
+                send_command("False")
+                print("No match")
+
+            face_names.append(name)
+    
+    process_this_frame = not process_this_frame
+
     for (top, right, bottom, left), face_encoding, known in zip(face_locations, face_encodings, known_face_encodings):
-        # matches uses compare faces function to compare face encodings shown with the preloaded face encodings with tolerance of 0.42
-        # The lower the number, the more accurate it is 
-
-        # top *= 4
-        # right *= 4
-        # bottom *= 4
-        # left *= 4
-
-        matches = face_recognition.compare_faces(known, face_encodings, tolerance=0.42)
-        name = "Unknown"
-
-        # If there's a match with the first face encoding, use the first one
-        # if not, use the known face with the smallest distance to the new face
-        face_distances = face_recognition.face_distance(known, face_encodings)
-        best_match_index = np.argmin(face_distances)
-        if matches[best_match_index]:
-            name = "Caleb"
-            send_command("True")
-            # if unlocked == False:
-            #     send_command("True")
-            #     unlocked = True
-        # elif unlocked == True:
-        #     read_serial()
-        else:
-            send_command("False")
-            print("No match")
+        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+        top *= 4
+        right *= 4
+        bottom *= 4
+        left *= 4
 
         # Draw a box around the face
         cv.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
@@ -143,9 +100,6 @@ while True:
     if len(face_encodings) == 0:
         send_command("No face")
 
-    process_this_frame = not process_this_frame
-
-    # print(unlocked)
     # Display the resulting image
     cv.imshow('Video', frame)
 
--- smart_door.py
+++ smart_door.py
@@ -1,45 +1,40 @@
 import board
-
 from digitalio import DigitalInOut, Direction, Pull
-
 import pwmio
-
 from adafruit_motor import servo
-
 import time
-
 import usb_cdc
 
+# Setting up the servo
+# angles = [0, 20, 40, 60, 80, 100, 120, 140, 160, 180]
+# opp_angles = [180, 160, 140, 120, 100, 80, 60, 40, 20, 0]
 pwm = pwmio.PWMOut(board.D5, duty_cycle= 2 ** 15, frequency=50)
 my_servo = servo.Servo(pwm)
 
+# Save the current time to check for timeouts
+# Default timeout is 5 seconds -> if no face is recognized for 5 seconds, the servo will lock the door
 startTime = time.monotonic()
-
-# angles = [0, 20, 40, 60, 80, 100, 120, 140, 160, 180]
-# opp_angles = [180, 160, 140, 120, 100, 80, 60, 40, 20, 0]
 
 # Access the default USB serial port
 uart = usb_cdc.console
 
+# Initialize the servo to the starting position
 my_servo.angle = 0
 
 while True:
-
-    # Read data from the computer
+    # Check if there is data available from the USB serial port
     if uart.in_waiting > 0:
         data = uart.readline().decode('utf-8').strip()
+
+        # Check the received data and control the servo accordingly
         if data == "True":
+            # The user is recognized
             my_servo.angle = 180
             print("Match")
             startTime = time.monotonic()
-        elif data == "False":
+        else:
+            # The user is not recognized or no face is detected
             print("No match")
-            if time.monotonic() - startTime > 5:
-                my_servo.angle = 0
-                print(time.monotonic() - startTime)
-                print("Time out")
-        elif data == "No face":
-            print("No face")
             if time.monotonic() - startTime > 5:
                 my_servo.angle = 0
                 print(time.monotonic() - startTime)
